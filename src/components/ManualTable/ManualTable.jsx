4a4274709d2fbda870ff0d67b60e64f0032bf120
yuri <ybalalaev@gmail.com>
2021-06-18 15:38:17 +0300

    resize columns

--- src/components/ManualTable/ManualTable.jsx
+++ src/components/ManualTable/ManualTable.jsx
@@ -1,40 +1,76 @@
 import { Row } from 'antd'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ManualTable.css'
 
 const ManualTable = ({ columns, data, select }) => {
   if (select) {
     columns = [{ title: '[x]', dataIndex: '#chbx' }, ...columns]
   }
-  console.log(columns)
+  const [resizedElement, setResizedElement] = useState({})
+
+  const onMouseDown = (event) => {
+    const resizer = event.target
+    const parent = resizer.closest('[data-type="resizable"]')
+    const coords = parent.getBoundingClientRect()
+    let valueX
+    let valueY
+    // setResizedElement(parent.dataset.id)
+    document.onmousemove = (e) => {
+      const deltaX = e.pageX - coords.right
+      valueX = coords.width + deltaX
+      parent.style.width = valueX + 'px'
+      console.log('>>', e.pageX)
+    }
+
+    document.onmouseup = () => {
+      document.onmousemove = null
+      document.onmouseup = null
+    }
+  }
+
+  useEffect(() => {
+    console.log(resizedElement)
+  }, [resizedElement])
 
   return (
     <table className='table'>
       <thead className='thead'>
-        <tr className='tr'>
+        <tr className='tr header'>
           {/* <th className={['th'].join(' ')}>[x]</th> */}
-          {columns.map((col) => {
-            return <th className='th'>{col.title}</th>
+          {columns.map((col, index) => {
+            return (
+              <th data-type='resizable' data-id={index} className={['th', 'resizable'].join(' ')} key={col?.dataIndex}>
+                {col?.title}
+                <div
+                  data-type='resizer'
+                  className='resizer'
+                  onMouseDown={onMouseDown}
+                  // onMouseMove={onMouseMove}
+                  // onMouseUp={onMouseUp}
+                  // onDrag={onDrag}
+                />
+              </th>
+            )
           })}
         </tr>
       </thead>
 
       <tbody className='tbody'>
         {data.map((row, idx) => {
-          if (typeof row === 'object' && Object.keys(row).length) {
+          if (typeof row === 'object' && Object.keys(row)?.length) {
             return (
-              <tr className='tr' key={row.id || idx}>
-                {columns.map((col, idx) => {
+              <tr className='tr' key={row?.id || idx}>
+                {columns.map((col, index) => {
                   return (
-                    <td className='td' key={idx}>
-                      {col.dataIndex === '#chbx' ? '[v]' : row[col.dataIndex]}
+                    <td data-type='resizable' data-id={index} className='td' key={col?.dataIndex || index}>
+                      {col?.dataIndex === '#chbx' ? '[v]' : row[col?.dataIndex]}
                     </td>
                   )
                 })}
               </tr>
             )
           } else {
-            return <tr>bad Data</tr>
+            return <tr key={row?.id || idx}>bad Data</tr>
           }
         })}
       </tbody>
